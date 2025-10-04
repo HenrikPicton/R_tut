17d28014f5db1961c2a68b6e5ff7837f4a646b9a
Henrik Picton <blaberstien@gmail.com>
2025-10-04 12:12:26 +0200

    oppgave 15 og 16

--- ggplot_tut.R
+++ ggplot_tut.R
@@ -8,3 +8,28 @@ Oppgave14 <- ggplot(data = strengthvolume %>%
   facet_wrap(~ sets)
 
 Oppgave14
+
+
+
+Oppgave15 <- strengthvolume %>%
+  filter(exercise %in% c("isom", "isok.60", "isok.120", "isok.240"),
+         !is.na(load)) %>%
+  mutate(velocity = case_when(
+    exercise == "isom" ~ 0,
+    exercise == "isok.60" ~ 60,
+    exercise == "isok.120" ~ 120,
+    exercise == "isok.240" ~ 240
+  )) %>%
+  ggplot(aes(x = velocity, y = load, color = sex)) +
+  geom_smooth(method = "lm", se = TRUE) +
+  facet_wrap(~ time)
+
+Oppgave15
+
+
+
+Oppgave16 <- millward %>% 
+  ggplot(aes(x = RNA, y = protein_synthesis))+
+  geom_smooth(method = "lm", se = TRUE)
+
+Oppgave16
